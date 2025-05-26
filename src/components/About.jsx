4f9a649d018312f5cf0c52e37cbe28e6a5764a45
nugiimac1 <emmanuelogar512@gmail.com>
2025-05-26 18:01:54 +0100

    Updates to hero, nav etc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,19 +30,18 @@ const About = () => {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
+          Hii, i'ts Mazetrekker
         </p>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          title="A cre<b>a</b>tive <b>c</b>hameleon with a flair<br /> stylish, <b>joyful, <b>and oh-so-clickable designs <b>"
           containerClass="mt-5 !text-black text-center"
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p> Whether I’m sketching wireframes or picking the perfect palette, </p>
           <p className="text-gray-500">
-            Zentry unites every player from countless games and platforms, both
-            digital and physical, into a unified Play Economy
+            I believe fun and function should always go hand-in-hand.
           </p>
         </div>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -141,22 +141,23 @@ const Hero = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b>A</b>MING
+          D<b>E</b>SINGER
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
-              redefi<b>n</b>e
+              {/* redefi<b>n</b>e */}
+              Design that Dares <b>to Be Different</b>
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame Layer <br /> Unleash the Play Economy
+              Hi, I’m Ogar Emmanuel a playful pixel-pusher turning boring interfaces into bold experiences.<br />From quirky branding to buttery-smooth UX, I design with personality and purpose.
             </p>
 
             <Button
               id="watch-trailer"
-              title="Watch trailer"
+              title="Let’s Make Magic"
               leftIcon={<TiLocationArrow />}
               containerClass="bg-yellow-300 flex-center gap-1"
             />
@@ -165,7 +166,7 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        G<b>A</b>MING
+        D<b>E</b>SING
       </h1>
     </div>
   );
